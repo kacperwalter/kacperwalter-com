9859cd8406d69bd1a18c64d39904f5a5f7775e7c
Kacper Walter <kacwal@poczta.fm>
2020-10-09 20:21:03 +0200

    add parallax

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// parallax stuff
+const parallax = (e) => {
+    document.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('data-speed');
+        const x = (window.innerWidth - e.pageX * speed) / 100;
+        const y = (window.innerHeight - e.pageY * speed) / 100;
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    })
+}
+
+document.addEventListener('mousemove', parallax);

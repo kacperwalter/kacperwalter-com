79d355680b732d1b3082ba32faa60364fe4ff059
Kacper Walter <kacwal@poczta.fm>
2020-10-10 16:00:40 +0200

    Add custom cursor

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,21 @@ const parallaxCondition = () => {
 }
 
 parallaxCondition();
+
+// cursor animating stuff
+const cursor = document.querySelector('.cursor');
+
+const cursorMove = (e) => {
+    cursor.style.top = (e.pageY - 10) + 'px';
+    cursor.style.left = (e.pageX - 10) + 'px';
+}
+
+const cursorExpand = () => {
+    cursor.classList.add('cursor-expand');
+    setTimeout(() => {
+        cursor.classList.remove('cursor-expand');
+    }, 500)
+}
+
+document.addEventListener('mousemove', cursorMove)
+document.addEventListener('click', cursorExpand)

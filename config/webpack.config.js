9c3efd663934268c2d6e6e5085f1a8b0b1ca5f82
Kacper Walter <kacwal@poczta.fm>
2021-03-14 17:31:57 +0100

    basic Webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,25 @@
 const path = require('path');
 
 const webpackConfig = {
+    mode: 'development',
+    entry: {
+        main: './src/app.js',
+    },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, '../', 'build'),
+    },
+    module: {
 
-}
+    },
+    plugins: {
+
+    },
+    devServer: {
+        open: true,
+        contentBase: path.resolve(__dirname, '..', 'public'),
+        port: 8080m
+    },
+};
 
 module.exports = webpackConfig;

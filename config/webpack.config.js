75936fe23af6701f6a69eb5b708c09a80b39ac52
Kacper Walter <kacwal@poczta.fm>
2021-03-14 17:56:52 +0100

    Webpack configured and working

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 const webpackConfig = {
     mode: 'development',
     entry: {
@@ -10,15 +13,31 @@ const webpackConfig = {
         path: path.resolve(__dirname, '../', 'build'),
     },
     module: {
-
+        rules: [
+            {
+                test: /\.(jpg|png|svg|gif|jpeg)$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ]
     },
-    plugins: {
-
-    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/templates/template.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
     devServer: {
         open: true,
         contentBase: path.resolve(__dirname, '..', 'public'),
-        port: 8080m
+        port: 8080,
     },
 };
 

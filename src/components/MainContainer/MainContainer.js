8a3a49c8f65165118db3b2c6260183b547175a41
Kacper Walter <kacper.walter@netguru.com>
2021-11-23 18:51:15 +0100

    add basic styles and component

---
+++ src/components/MainContainer/MainContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Root from 'views/Root';
+
+const MainContainer = () => (
+  <div>
+    <div>Works perfect!</div>
+  </div>
+);
+
+export default MainContainer;
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import MainContainer from 'components/MainContainer/MainContainer';
 
-const Root = () => <div>Works perfect!</div>;
+const Root = () => (
+  <>
+    <MainContainer />
+  </>
+);
 
 export default Root;

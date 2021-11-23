d9f0b4885df989afc53de31a9bb57ad2f63b0fea
Kacper Walter <kacper.walter@netguru.com>
2021-11-23 18:25:17 +0100

    setup done

---
+++ src/data/texts.js
@@ -0,0 +1,1 @@
+export const mainParagraph = `Jestem web developerem i UI designerem, specjalizującym się w tworzeniu witryn internetowych w oparciu o nowoczesne standardy. Zapraszam do współpracy!`;

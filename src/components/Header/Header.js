7fff18c3a504f84993db2b59479a50c30db8040a
Kacper Walter <kacper.walter@netguru.com>
2021-11-24 22:22:44 +0100

    Header styled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,10 @@ import styles from './Header.module.scss';
 import logo from 'assets/kacperwalter-logo.svg';
 
 const Header = (props) => (
-  <header className={`layer`}>
+  <header className={`${styles.Header}`}>
     <section>
-      <img src={logo} alt="Logo" />
-      <h1 className={`${styles.headerText} layer`}>{props.text}</h1>
+      <img className={`${styles.Logo}`} src={logo} alt="Logo" />
+      <h1 className={`${styles.HeaderText}`}>{props.text}</h1>
     </section>
   </header>
 );

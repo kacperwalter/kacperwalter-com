575c9fa17b33f5d3e4cba2860d9736c2087e99c4
kacperwalter <kacwal@poczta.fm>
2023-08-29 11:45:53 +0200

    Add parallax and things

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,30 @@
 import styles from './Hero.module.scss'
-import kwLogo from '../../../public/kacperwalter-logo.svg'
+import kwLogo from '../../assets/kacperwalter-logo.svg'
+import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
 
 const Hero = () => {
   return (
-    <section className={styles.hero}>
-      <header className={styles.header}>
-        <img className={styles.logo} src={kwLogo} />
-        <h1 className={styles.heading}>Frontend software developer</h1>
-      </header>
+    <MouseParallaxContainer globalFactorX={0.1} globalFactorY={0.1}>
+      <section className={styles.hero}>
+      <MouseParallaxChild factorX={0.3} factorY={0.5}>
+        <header className={styles.header}>
+          <img className={styles.logo} src={kwLogo} />
+          <h1 className={styles.heading}>Frontend software developer</h1>
+        </header>
+      </MouseParallaxChild>
 
-      <article className={styles.description}>
-        <h2 className={styles.caption}>
-          Hi there 👋 I'm Kacper and I am front-ending for a living. Let's get in touch!
-        </h2>
-        <a className={styles.link} href="#">GitHub</a>
-        <a className={styles.link} href="#">Linkedin</a>
-        <a className={styles.link} href="#">Instagram</a>
-      </article>
-    </section>
+      <MouseParallaxChild factorX={0.6} factorY={0.2}>
+        <article className={styles.description}>
+          <h2 className={styles.caption}>
+            Hi there 👋 I'm Kacper and I am front-ending for a living. Let's get in touch!
+          </h2>
+          <a className={styles.link} href="https://github.com/kacperwalter">GitHub</a>
+          <a className={styles.link} href="https://www.linkedin.com/in/kacper-walter/">Linkedin</a>
+          <a className={styles.link} href="https://instagram.com/wacperkalter?igshid=OGQ5ZDc2ODk2ZA==">Instagram</a>
+        </article>
+      </MouseParallaxChild>
+      </section>
+    </MouseParallaxContainer>
   )
 }
 

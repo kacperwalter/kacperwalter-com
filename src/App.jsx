f6c5eea2398c5c406b48b09cd5781dc6851ba5db
kacperwalter <kacwal@poczta.fm>
2023-08-29 11:31:04 +0200

    Add animated cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import Hero from './components/Hero/Hero'
+import AnimatedCursor from "react-animated-cursor"
+
+// TODO
+// Animated cursor -> you cen differentiate between themes so the cursor can change the color depending on parent element
+// check out docs -> https://github.com/stephenscaff/react-animated-cursor#-features
 
 function App() {
 
   return (
     <>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={30}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)'
+        }}
+        outerStyle={{
+          border: '2px solid var(--cursor-color)'
+        }}
+      />
       <Hero />
     </>
   )

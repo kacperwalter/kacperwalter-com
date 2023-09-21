338ad0382a79eb62f0b0980df6079a3bbe201962
kacperwalter <kacwal@poczta.fm>
2023-09-21 12:22:44 +0200

    Change copy

--- data/heroContent.js
+++ data/heroContent.js
@@ -1,6 +1,6 @@
 export const imageAlt = 'kacperwalter.com logo'
 export const heading = 'Frontend software developer'
-export const caption = `Hi there 👋🏻 I'm Kacper, software developer who is love with beautiful UI's & low-level web development. Currently freelancing, let's stay in touch!`
+export const caption = `Hello there 👋🏻 I'm Kacper, a software developer who absolutely loves low level web dev and crafting beautiful UI's. I'm currently freelancing, so let's stay in touch!`
 export const links = [
   { id: 1, href: 'https://github.com/kacperwalter', text: 'GitHub' },
   { id: 2, href: 'https://www.linkedin.com/in/kacper-walter/', text: 'Linkedin' },

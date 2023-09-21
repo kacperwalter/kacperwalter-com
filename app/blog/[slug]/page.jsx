8555b27296ee411b1b85b4ad93da736acc09107e
kacperwalter <kacwal@poczta.fm>
2023-09-21 18:28:17 +0200

    Add images handling in PortabeText

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,6 +1,9 @@
 const { client } = require("@/app/lib/sanity")
 
 import Navbar from "@/app/components/Navbar/Navbar"
+import { urlFor } from "@/app/lib/sanityImageUrl"
+import { PortableText } from "@portabletext/react"
+import Image from "next/image"
 
 const getData = async (slug) => {
   const query = `*[_type == "post" && slug.current == "${slug}"][0]`
@@ -11,12 +14,21 @@ const getData = async (slug) => {
 // TODO to different compoenent 
 const BlogPost = async ({ params }) => {
   const data = await getData(params.slug);
+  
+  const PortableTextComponent = {
+    types: {
+      image: ({value}) => (
+        <Image src={urlFor(value).url()} alt='some alt' width={800} height={800}/>
+      )
+    }
+  }
 
   return (
     <>
       <Navbar />
       <section>
         <h1>{data.title}</h1>
+        <PortableText value={data.content} components={PortableTextComponent} />
       </section>
     </>
   )
